ee56f6bc6fc8f3a662ef82914fe212d09d53e5c0
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2023-01-06 08:45:05 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'vivr2.0-revamp';
+}
