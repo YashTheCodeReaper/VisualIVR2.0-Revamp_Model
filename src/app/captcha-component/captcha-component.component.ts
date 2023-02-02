c6d95340ce5c07a793f2ec50b2088557f0d4fab0
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2023-02-02 20:00:18 +0530

    23423

--- src/app/captcha-component/captcha-component.component.ts
+++ src/app/captcha-component/captcha-component.component.ts
@@ -64,7 +64,7 @@ export class CaptchaComponentComponent implements OnInit {
       },
       captchaResetButtonPosition: 'right',
       separatedInputs: {
-        enable: true,
+        enable: false,
         letterCount: 4
       },
       captchaFooter: {
